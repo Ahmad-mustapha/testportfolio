4842ad25d782bfd8ea5f49995b07dc07fe64180f
Ahmad-mustapha <ahmadishola12@gmail.com>
2024-09-06 12:16:18 -0700

    fixes

--- src/component/import.js
+++ src/component/import.js
@@ -1,5 +1,5 @@
 export {default as Navbar} from './navbar/Navbar'
-export {default as Project} from './project/Project'
+export {default as Projects} from './project/Projects'
 export {default as Service} from './service/Service'
 export {default as fact} from './fact/Fact'
 export {default as Footer} from './footer/Footer'
